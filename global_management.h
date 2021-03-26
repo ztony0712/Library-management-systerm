13f519bc93f1d96ba84eea3d60b87e3ec58e2cae
ztony0712 <ztony0712@outlook.com>
2021-03-26 12:54:08 +0800

    my_gets(), init() and end()

--- global_management.h
+++ global_management.h
@@ -4,12 +4,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define puts(s) fputs(s, stdout)
 
-char *my_gets(void);
+char *my_gets (void);
 
 void init (void);
 
 void end (void);
+
+
 
 #endif
 
